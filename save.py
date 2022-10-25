6ddb675e51d9004111273392fb349d64714fb6e4
Cody Gardner <35705693+cagardner@users.noreply.github.com>
2022-10-24 20:11:02 -0700

    Add files via upload

---
+++ save.py
@@ -0,0 +1,29 @@
+from PIL import ImageGrab   #pip install image
+import cv2  #pip install opencv-ptyhon
+import numpy as np  #pip install numpy 
+import keyboard #pip install keyboard
+import time     #pip install time
+
+
+x = 0
+flag = False
+def TakePicture():
+    screenshot = np.array(ImageGrab.grab(bbox=(640,220,1280,860)))
+    screenshot = cv2.cvtColor(screenshot,cv2.COLOR_BGR2RGB)
+    cv2.imwrite(file_name , screenshot)
+    
+    
+
+while not flag:
+    try:
+        if keyboard.is_pressed('c'):
+            
+            file_name = str(x) + "_2z.jpg"
+            TakePicture()
+            x = x + 1
+            time.sleep(0.1)
+        elif keyboard.is_pressed('z'):
+            flag = True
+    except:
+        print("Game")
+print('Code exits with',flag,'\nWe are eternally grateful')
